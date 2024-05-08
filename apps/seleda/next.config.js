156c706b82d44409d817a6c7204bcc9392c70531
biniamkiros <biniamdev@gmail.com>
2024-05-08 22:01:09 +0000

    seleda

--- apps/seleda/next.config.js
+++ apps/seleda/next.config.js
@@ -2,9 +2,21 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+// await import("./src/env.js");
 
-/** @type {import("next").NextConfig} */
-const config = {};
+// /** @type {import("next").NextConfig} */
+// const config = {};
 
-export default config;
+// export default config;
+
+
+const path = require("path");
+
+module.exports = {
+  reactStrictMode: true,
+  transpilePackages: ["@repo/ui"],
+  output: "standalone",
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
+};
