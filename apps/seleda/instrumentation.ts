5aa0bd1295fa1701789bce36f2d602714af6e690
biniamkiros <biniamdev@gmail.com>
2024-05-24 09:26:21 +0000

    seleda

---
+++ apps/seleda/instrumentation.ts
@@ -0,0 +1,8 @@
+export async function register() {
+    console.log("*************************")
+    console.log("*************************")
+    console.log("🚀 ~ register ~ register:")
+    console.log("*************************")
+    console.log("*************************")
+    
+}
