47743ea1eae94cc34d0185e971e945d5fbb247ab
biniamkiros <biniamdev@gmail.com>
2024-05-24 09:47:53 +0000

    seleda

--- apps/seleda/src/instrumentation.ts
+++ apps/seleda/src/instrumentation.ts
@@ -1,11 +1,11 @@
 export async function register() {
     console.log("!!!!!!REGISTER", process.env.NEXT_RUNTIME)
     const isServer = process.env.NEXT_RUNTIME === "nodejs";
-    setInterval(() =>{
+    
     console.log("*************************")
     console.log("*************************")
     console.log("🚀 ~ register ~ register:")
     console.log("*************************")
     console.log("*************************")
-    })
+    
 }
