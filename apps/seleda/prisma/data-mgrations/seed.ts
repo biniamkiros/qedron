966c47a8717eff1f9ecdba34b4920f27bca49af7
biniamkiros <biniamdev@gmail.com>
2024-06-03 16:32:43 +0000

    seleda

--- apps/seleda/prisma/data-mgrations/seed.ts
+++ apps/seleda/prisma/data-mgrations/seed.ts
@@ -1,33 +1,33 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-const main = async () => {
-  try {
-    await prisma.products.deleteMany();
-    const email = faker.internet.email(
-      "ishan",
-      "manandhar",
-      "example.fakerjs.dev"
-    );
+// const main = async () => {
+//   try {
+//     await prisma.products.deleteMany();
+//     const email = faker.internet.email(
+//       "ishan",
+//       "manandhar",
+//       "example.fakerjs.dev"
+//     );
 
-    // cleanup the existing database
-    await prisma.user.delete({ where: { email } }).catch(() => {
-      // delete existing user found with same email
-    });
+//     // cleanup the existing database
+//     await prisma.user.delete({ where: { email } }).catch(() => {
+//       // delete existing user found with same email
+//     });
 
-    await prisma.user.create({
-      data: {
-        name: "ishan",
-      },
-    });
+//     await prisma.user.create({
+//       data: {
+//         name: "ishan",
+//       },
+//     });
 
-    console.log(`Database has been seeded. 🌱`);
-  } catch (error) {
-    throw error;
-  }
-};
+//     console.log(`Database has been seeded. 🌱`);
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
-main().catch((err) => {
-  console.warn("Error While generating Seed: \n", err);
-});
+// main().catch((err) => {
+//   console.warn("Error While generating Seed: \n", err);
+// });
