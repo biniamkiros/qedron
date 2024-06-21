fc4d61d60506283dc40165e2f7d81d88947ce26c
biniamkiros <biniamdev@gmail.com>
2024-06-21 15:02:49 +0000

    seleda

--- apps/seleda/src/app/api/chapa/confirm/route.ts
+++ apps/seleda/src/app/api/chapa/confirm/route.ts
@@ -26,10 +26,11 @@ export async function POST(request: NextRequest) {
     tx_ref,
     payment_method,
   } = body;
-  notifyAdmin(
-    `Payment from ${tx_ref} ${amount} signature: ${request.headers.get("Chapa-Signature")} x-signature: ${request.headers.get("Chapa-Signature")}`
-  );
-  if (hash == request.headers.get("Chapa-Signature")) {
+
+  if (
+    hash === request.headers.get("Chapa-Signature") ||
+    hash === request.headers.get("x-chapa-signature")
+  ) {
     // const v = {
     //   event: "charge.success",
     //   first_name: "ቢኒያም",
@@ -50,21 +51,27 @@ export async function POST(request: NextRequest) {
     //   customization: { title: null, description: null, logo: null },
     //   meta: "null",
     // };
-    const {
-      account_name, //: "Abebe Bikila",
-      account_number, //: "1000XXXXXXXXXXXX",
-      bank_id, //: "96e41186-29ba-4e30-b013-2ca36d7e7025",
-      bank_name, //: "Commercial Bank of Ethiopia (CBE)",
-      currency, //: "ETB",
-      amount, //: "1.00",
-      type, //: "Payout",
-      status, //: "success",
-      reference, //: "3241342142sabcdfdd",
-      chapa_reference, //: "3241342142sabcdfdd",
-      created_at, //: "2023-02-02T07:53:28.000000Z",
-    } = body;
-    const chatId = reference.slice("-")[0];
-    notifyAdmin(`Payment from ${chatId} ${amount}`);
+    // const {
+    //   account_name, //: "Abebe Bikila",
+    //   account_number, //: "1000XXXXXXXXXXXX",
+    //   bank_id, //: "96e41186-29ba-4e30-b013-2ca36d7e7025",
+    //   bank_name, //: "Commercial Bank of Ethiopia (CBE)",
+    //   currency, //: "ETB",
+    //   amount, //: "1.00",
+    //   type, //: "Payout",
+    //   status, //: "success",
+    //   reference, //: "3241342142sabcdfdd",
+    //   chapa_reference, //: "3241342142sabcdfdd",
+    //   created_at, //: "2023-02-02T07:53:28.000000Z",
+    // } = body;
+    // const chatId = reference.slice("-")[0];
+    // notifyAdmin(`Payment from ${chatId} ${amount}`);
+    notifyAdmin(
+      `Success Payment hash ${hash} 
+      from ${tx_ref} ${amount} 
+      signature: ${request.headers.get("Chapa-Signature")} 
+      x-signature: ${request.headers.get("x-chapa-signature")}`
+    );
     return NextResponse.json({ messsage: "confirmed" }, { status: 200 });
   } else {
     const {
@@ -81,7 +88,12 @@ export async function POST(request: NextRequest) {
       created_at, //: "2023-02-02T07:53:28.000000Z",
     } = body;
     const chatId = reference.slice("-")[0];
-
+    notifyAdmin(
+      `Error Payment hash ${hash} 
+      from ${tx_ref} ${amount} 
+      signature: ${request.headers.get("Chapa-Signature")} 
+      x-signature: ${request.headers.get("x-chapa-signature")}`
+    );
     NextResponse.json({ messsage: "error" }, { status: 400 });
   }
 }
